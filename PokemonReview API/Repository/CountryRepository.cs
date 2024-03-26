e07a38c0b09a7a77aca5562793268f1214ae5e14
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-26 21:46:12 +0530

    complete country repository

---
+++ PokemonReview API/Repository/CountryRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using PokemonReview_API.Data;
+using PokemonReview_API.Interfaces;
+using PokemonReview_API.Models;
+
+namespace PokemonReview_API.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public CountryRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public bool CountryExists(int id)
+        {
+            return _context.Countries.Any(c => c.Id == id);
+        }
+
+  
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountry(int id)
+        {
+            return _context.Countries.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromACountry(int countryId)
+        {
+            return _context.Owners.Where(c => c.Country.Id == countryId).ToList();
+        }
+
+    }
+}
