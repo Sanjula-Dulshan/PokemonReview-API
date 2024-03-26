d6e8ebf15f36a1fb14f6c1e0037fda8210489bd9
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-26 15:39:30 +0530

    implement category repository

---
+++ PokemonReview API/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReview_API.Models;
+
+namespace PokemonReview_API.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+
+        bool CategoryExists(int id);
+    }
+}
---
+++ PokemonReview API/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using PokemonReview_API.Data;
+using PokemonReview_API.Interfaces;
+using PokemonReview_API.Models;
+
+namespace PokemonReview_API.Repository
+{
+
+    public class CategoryRepository : ICategoryRepository
+    {
+        private DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(c => c.Id == id);
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(e=> e.CategoryId == categoryId).Select(c=>c.Pokemon).ToList();
+        }
+    }
+}
