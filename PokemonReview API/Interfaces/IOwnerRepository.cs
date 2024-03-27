2fd52ebb932c9b9c50257dd452b06ca3163f385e
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-27 11:27:40 +0530

    create owner repository

---
+++ PokemonReview API/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReview_API.Models;
+
+namespace PokemonReview_API.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+    }
+}
